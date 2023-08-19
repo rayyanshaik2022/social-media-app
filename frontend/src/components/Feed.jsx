710f71f8f28b9039e3e342e29cfb9c23287e8dd5
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2023-08-19 14:33:44 -0400

    Fixed some media query issues

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -3,13 +3,20 @@ import { Flex } from "@chakra-ui/react";
 import NewPost from "./NewPost";
 
 function Feed() {
-    return (
-        <>
-        <Flex flexDir={"column"} bg={"gray.50"} minH={"calc(100vh - 110px)"} boxShadow={"2xl"} padding={{base: 2, sm: 10}} zIndex={2}>
-            <NewPost />
-        </Flex>
-        </>
-    );
+  return (
+    <>
+      <Flex
+        flexDir={"column"}
+        bg={"gray.50"}
+        minH={"calc(100vh - 110px)"}
+        boxShadow={"2xl"}
+        padding={{ base: 2, sm: 10 }}
+        zIndex={2}
+      >
+        <NewPost />
+      </Flex>
+    </>
+  );
 }
 
 export default Feed;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,17 +4,31 @@ import SideBar from "../components/Sidebar";
 import Feed from "../components/Feed";
 import RightFeed from "../components/RightFeed";
 
+import { useMediaQuery } from "@chakra-ui/react";
+
 function Home() {
+  const [isLargerThan1280] = useMediaQuery("(min-width: 1280px)");
 
   return (
     <>
       <Box w={"100vw"} h={"100vh"}>
         <Flex flexDir={"column"}>
           <Navbar />
-          <Grid w={"100%"} bg={"red"} gridTemplateColumns={{base: "0px 1fr", md:"200px 1fr", xl: "200px 1fr 440px"}}>
+          <Grid
+            w={"100%"}
+            bg={"red"}
+            gridTemplateColumns={{
+              base: "0px 1fr 0px",
+              md: "200px 1fr 0px",
+              xl: "200px 1fr 400px",
+            }}
+          >
             <SideBar />
             <Feed />
-            <RightFeed />
+            {
+                isLargerThan1280 ? <RightFeed /> : null
+            }
+            
           </Grid>
         </Flex>
       </Box>
