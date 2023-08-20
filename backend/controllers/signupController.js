3ab382c2c1eeeff47ea417057f4ec3d57411ec3f
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2023-08-20 10:10:55 -0400

    Update sign up to include jwt

--- backend/controllers/signupController.js
+++ backend/controllers/signupController.js
@@ -1,21 +1,27 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/user");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const  Refresh = require('../model/refresh');
-require("dotenv")
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Refresh = require("../model/refresh");
+require("dotenv");
 
-const jwtSecret  = process.env.JWT_SECRET;
+const jwtSecret = process.env.JWT_SECRET;
+const jwtExpiresIn = process.env.JWT_EXPIRES_IN
 
 exports.signup_create_post = asyncHandler(async (req, res, next) => {
-  console.log(req.body);
+  const hashedPassword = await bcrypt.hash(password, 8);
+
   const user = new User({
     username: req.body.username,
     password: req.body.password,
     displayName: req.body.username,
   });
-
   await user.save();
-  console.log("user created!");
-  res.json({ response: "sent!" });
+
+  const token = jwt.sign({ _id: user._id, username: user.username }, jwtSecret, {
+    expiresIn: jwtExpiresIn,
+  });
+
+  res.send({ token })
+  
 });
