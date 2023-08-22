55cb90f06e03504be13fc737d4e569fa0941345b
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2023-08-22 02:25:54 -0400

    changed url domain

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,2 @@
 export const SERVER_URL = "https://social-media-backend-jxeo.onrender.com"
-export const WEBSITE_URL = "https://505a5d17.social-media-app-3k8.pages.dev/"
+export const WEBSITE_URL = "https://social-media-app-3k8.pages.dev/"
